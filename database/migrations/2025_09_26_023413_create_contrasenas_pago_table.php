0fd2983f5f1d6a999b0bac0931921ca254c853a9
Estefany Natali Velasquez Yox <natalivelasquez@MacBook-Air-de-Estefany.local>
2025-09-30 23:31:30 -0600

    fix: contraseñas pago migration

---
+++ database/migrations/2025_09_26_023413_create_contrasenas_pago_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+      if (!Schema::hasTable('contrasenas_pago')) { 
+            Schema::create('contrasenas_pago', function (Blueprint $table) {
+                $table->id();
+                $table->unsignedBigInteger('factura_id');
+                $table->string('codigo');
+                $table->string('archivo')->nullable();
+                $table->date('fecha_documento');
+                $table->date('fecha_aprox')->nullable();
+                $table->enum('estado', ['pendiente', 'validada'])->default('pendiente');
+                $table->timestamp('validada_en')->nullable();
+                $table->timestamps();
+
+                $table->foreign('factura_id')->references('id')->on('facturas')->onDelete('cascade');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+       Schema::dropIfExists('contrasenas_pagos');
+    }
+};
--- database/migrations/2025_10_01_035746_add_fecha_aprox_to_contrasenas_pago_table.php
+++ database/migrations/2025_10_01_035746_add_fecha_aprox_to_contrasenas_pago_table.php
@@ -11,9 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('contrasenas_pago', function (Blueprint $table) {
-             $table->date('fecha_aprox')->nullable()->after('fecha_documento');
-        });
+        if (Schema::hasTable('contrasenas_pago') && !Schema::hasColumn('contrasenas_pago', 'fecha_aprox')) {
+            Schema::table('contrasenas_pago', function (Blueprint $table) {
+                $table->date('fecha_aprox')->nullable()->after('fecha_documento');
+            });
+        }
     }
 
     /**
